5f4f8063b01c6debdd41f7ba3b7a7eb8b06a0f83
GO IT Teacher <pikimelposhta@gmail.com>
2023-02-22 07:47:28 +0200

    update

--- src/js/01-script.js
+++ src/js/01-script.js
@@ -1,23 +1,5 @@
 import '../css/common.css';
 import { refs } from './modules/refs.js';
-import { getHeroByName, heroMarkup } from './modules/heroes';
-import { getWeather } from './modules/weather';
-
-refs.form.addEventListener('submit', e => {
-  e.preventDefault();
-  const name = e.target.elements.query.value;
-  const result = getHeroByName(name);
-
-  result.then(hero => {
-    refs.cardContainer.innerHTML = heroMarkup(hero);
-  });
-});
-
-refs.form1.addEventListener('submit', e => {
-  e.preventDefault();
-
-  getWeather(Math.random() * 30, 30, 'uk');
-});
 
 // =======================================================
 // =======================================================
